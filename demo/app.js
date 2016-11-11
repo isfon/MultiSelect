bd16d68bb08624b8065e588ed6c475f11f967677
ifonseca <lamgers@gmail.com>
2016-11-11 16:26:24 -0600

    beta finish

---
+++ demo/app.js
@@ -0,0 +1,35 @@
+function demoController (){
+	this.options = {
+	    searchPlaceHolder: 'Typing to filter.',
+	    labelAll: 'All Countries',
+	    labelSelected: 'Selected Countries',
+	    orderProperty: 'name',
+	    items: [
+			{"name": "Afghanistan", "code": "AF"},
+			{"name": "Åland Islands", "code": "AX"},
+			{"name": "Albania", "code": "AL"},
+			{"name": "Algeria", "code": "DZ"},
+			{"name": "Martinique", "code": "MQ"},
+			{"name": "Mauritania", "code": "MR"},
+			{"name": "Mauritius", "code": "MU"},
+			{"name": "Mayotte", "code": "YT"},
+			{"name": "Uzbekistan", "code": "UZ"},
+			{"name": "Vanuatu", "code": "VU"},
+			{"name": "Venezuela", "code": "VE"},
+			{"name": "Viet Nam", "code": "VN"},
+			{"name": "Virgin Islands, British", "code": "VG"},
+			{"name": "Virgin Islands, U.S.", "code": "VI"},
+			{"name": "Wallis and Futuna", "code": "WF"},
+			{"name": "Western Sahara", "code": "EH"},
+			{"name": "Yemen", "code": "YE"},
+			{"name": "Zambia", "code": "ZM"},
+			{"name": "Zimbabwe", "code": "ZW"}
+		],
+	    selectedItems: [] 
+	};
+}
+
+
+angular
+	.module('app',['multiselectcomp'])
+	.controller('demoController', demoController);
