5f69cb8e3ad63233131eba7796444e82a4e05f44
isfon <lamgers@gmail.com>
2017-01-05 17:32:52 -0600

    change to templateurl

--- dist/js/multi-select.js
+++ dist/js/multi-select.js
@@ -26,37 +26,5 @@ angular
 	               }
 	        }
 		},
-		template: `
-			<div class="multiSelect">
-		       <div class="row">
-		        <div class="col-lg-5 col-md-5 col-sm-5">
-		          <input class="form-control" placeholder="{{$ctrl.options.searchPlaceHolder}}" ng-model="$ctrl.searchItem">
-		        </div>
-		       </div>
-		       <div class="row">
-		        <div class="col-lg-6 col-md-6 col-sm-6">
-		          <label>{{$ctrl.options.labelAll}}</label>
-		          <button type="button" class="btn btn-warning btn-xs" ng-click="$ctrl.change($ctrl.options.items, $ctrl.options.selectedItems, -1)">Select All</button>
-		          <div class="pool">
-		            <ul>
-		              <li ng-repeat="item in $ctrl.options.items | filter: $ctrl.searchItem | orderBy: $ctrl.options.orderProperty">
-		                <a href="" ng-click="$ctrl.change($ctrl.options.items, $ctrl.options.selectedItems, $ctrl.options.items.indexOf(item))">{{item[$ctrl.options.labelShow]}}</a>
-		              </li>
-		            </ul>
-		          </div>
-		        </div>
-		        <div class="col-lg-6 col-md-6 col-sm-6">
-		          <label>{{$ctrl.options.labelSelected}}</label>
-		          <button type="button" class="btn btn-warning btn-xs" ng-click="$ctrl.change($ctrl.options.selectedItems, $ctrl.options.items, -1)">Deselect All</button>
-		          <div class="pool">
-		            <ul>
-		              <li ng-repeat="item in $ctrl.options.selectedItems | orderBy: $ctrl.options.orderProperty">
-		                <a href="" ng-click="$ctrl.change($ctrl.options.selectedItems, $ctrl.options.items, $ctrl.options.selectedItems.indexOf(item))">{{item[$ctrl.options.labelShow]}}</a>
-		              </li>
-		            </ul>
-		          </div>
-		        </div>
-		       </div>
-		    </div>
-		`
+		templateUrl: 'dist/view/multiSelect.html'
 	});
